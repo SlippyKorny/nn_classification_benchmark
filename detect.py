18c4c19f8ae3e1bd5d657f7fe0f1d1ff61f34f11
Kornel Domeradzki <korneldomeradzki1@gmail.com>
2020-08-11 12:54:02 +0200

    Added support for training InceptionResNetV2 and InceptionV3

--- detect.py
+++ detect.py
@@ -1,23 +1,27 @@
 import argparse
 import tensorflow as tf
-from tensorflow import keras
 from keras.models import model_from_json
-from tensorflow.keras import layers
 
 import numpy as np
 from PIL import Image
 
 # Arguments
-parser = argparse.ArgumentParser(description='Checks if the parking spot was taken with the use of the passed model and weights file')
+parser = argparse.ArgumentParser(
+    description='Checks if the parking spot was taken with the use of the passed model and weights file')
 
-parser.add_argument('-m', '--model', dest = 'model_path', default = "model.json", 
-                    help = 'path to the model file (default: "model.json")')
+parser.add_argument('-m', '--model', dest='model_path', default="model.json",
+                    help='path to the model file (default: "model.json")')
 
-parser.add_argument('-w', '--weights', dest = 'weights_path', default = "model.h5",
-                    help = 'path to the weights file (default: "model.h5")')
+parser.add_argument('-w', '--weights', dest='weights_path', default="model.h5",
+                    help='path to the weights file (default: "model.h5")')
 
-parser.add_argument('-i', '--image', dest = 'img_path', default = "image.jpg", 
-                    help = 'path to the image that should be classified (default: "image.jpg")')
+parser.add_argument('--base', dest='model_base_name',
+                    default="MobileNetV2",
+                    help='name of the base of the neural network model (default: "MobileNetV2")')
+
+parser.add_argument('-i', '--image', dest='img_path', default="image.jpg",
+                    help='path to the image that should be classified (default: "image.jpg")')
+
 
 def get_model(model_path, weights_path):
     json_model = None
@@ -30,10 +34,11 @@ def get_model(model_path, weights_path):
 
     base_learning_rate = 0.0001
     model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
     print("compiled the model")
     return model
+
 
 # Main
 def main():
@@ -41,7 +46,7 @@ def main():
     model = get_model(args.model_path, args.weights_path)
     image = np.array(Image.open(args.img_path)) / 255.0
     print("min: ", np.min(image), "max:", np.max(image))
-    
+
     prediction = model.predict(np.array([image]))
     predicted_class = None
     if prediction > 0:
@@ -49,6 +54,7 @@ def main():
     else:
         predicted_class = "Free"
     print("Prediction: ", predicted_class)
+
 
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -1,37 +1,57 @@
 import argparse
 import pathlib
 import tensorflow as tf
-import tensorflow_datasets as tfds
 from tensorflow import keras
-from keras import layers
-
-# keras = tf.keras
 
 # Arguments
-parser = argparse.ArgumentParser(description='Trains a convolutional neural network model with the preprocessed CNR-EXT dataset and exports it')
+parser = argparse.ArgumentParser(
+    description='Trains a neural network model with a dataset and exports it')
 
-parser.add_argument('--output', dest = 'file_name', 
-                    default = "model", 
-                    help = 'name of the json and h5 files to which the cnn model will be exported (default: "model")')
+parser.add_argument('--output', dest='file_name',
+                    default="model",
+                    help='name of the json and h5 files to which the neural network\'s model will be exported ('
+                         'default: "model")')
 
-parser.add_argument('--model', dest = 'model_name', 
-                    default = "MobileNetV2", 
-                    help = 'name of the cnn model (default: "MobileNetV2")')
+parser.add_argument('--epochs', dest='epochs',
+                    default="20",
+                    help='number of training epochs (default: 20)')
 
-parser.add_argument('--data-dir', dest = 'dataset_directory', 
-                    default = "dataset/", 
-                    help = 'directory of the dataset used for training (default: "dataset/")')
+parser.add_argument('--classes', dest='class_count',
+                    default="1",
+                    help='number of classes (default: 1)')
+
+parser.add_argument('--model', dest='model_name',
+                    default="MobileNetV2",
+                    help='name of the neural network\'s model (default: "MobileNetV2")')
+
+parser.add_argument('--data-dir', dest='dataset_directory',
+                    default="dataset/",
+                    help='directory of the dataset used for training (default: "dataset/")')
+
 
 # Creates a mobilenet v2 base model
-def get_mobilenet_v2_model(IMG_SIZE):
-    IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
-    base_model = tf.keras.applications.MobileNetV2(input_shape = IMG_SHAPE,
-        include_top = False, weights = 'imagenet')
+def get_mobilenet_v2_model(img_shape):
+    base_model = tf.keras.applications.MobileNetV2(input_shape=img_shape,
+                                                   include_top=False, weights='imagenet')
     base_model.trainable = False
     return base_model
 
-def foo():
-    return 
+
+def get_inception_res_net_v2_model(img_shape):
+    base_model = tf.keras.applications.InceptionResNetV2(input_shape=img_shape,
+                                                         include_top=False, weights='imagenet')
+    base_model.trainable = False
+
+    return base_model
+
+
+def get_inception_v3_model(img_shape):
+    base_model = tf.keras.applications.InceptionV3(input_shape=img_shape,
+                                                   include_top=False, weights='imagenet')
+    base_model.trainable = False
+
+    return base_model
+
 
 # Loads the dataset and returns a tuple of train data and validation data
 def load_dataset(data_dir, img_height, img_width, batch_size):
@@ -40,51 +60,56 @@ def load_dataset(data_dir, img_height, img_width, batch_size):
         seed=123, image_size=(img_height, img_width),
         batch_size=batch_size)
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir, validation_split = 0.2, subset = "validation",
-        seed = 123, image_size = (img_height, img_width),
-        batch_size = batch_size)
+        data_dir, validation_split=0.2, subset="validation",
+        seed=123, image_size=(img_height, img_width),
+        batch_size=batch_size)
 
     # Scale the RGB values to 0-1.0 range
-    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
     train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
     val_ds = val_ds.map(lambda x, y: (normalization_layer(x), y))
 
     # Cache and prefetch data for better training performance
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    autotune_buffer_size = tf.data.experimental.AUTOTUNE
 
-    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    train_ds = train_ds.cache().prefetch(buffer_size=autotune_buffer_size)
+    val_ds = val_ds.cache().prefetch(buffer_size=autotune_buffer_size)
 
+    return train_ds, val_ds
 
-    return (train_ds, val_ds)
 
 # Creates the classifier and applies it to the base model
-def apply_classifier(base_model):
+def apply_classifier(base_model, class_count):
     global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-    prediction_layer = keras.layers.Dense(1)
+    prediction_layer = keras.layers.Dense(class_count)
     model = tf.keras.Sequential([
         base_model, global_average_layer,
         prediction_layer
     ])
     return model
 
+
 # Main
 def main():
     args = parser.parse_args()
-    IMG_SHAPE = 75
+    img_size = 75
+    img_shape = (img_size, img_size, 3)
     BATCH_SIZE = 128
+    class_count = int(args.class_count)
+    epochs = int(args.epochs)
 
     # Load the data
     data_dir = pathlib.Path(args.dataset_directory)
-    train_ds, val_ds = load_dataset(data_dir, IMG_SHAPE, IMG_SHAPE, BATCH_SIZE)
+    train_ds, val_ds = load_dataset(data_dir, img_size, img_size, BATCH_SIZE)
 
     # Load the base model and then apply the classifier
     model_switcher = {
-        "MobileNetV2": get_mobilenet_v2_model(IMG_SHAPE)#,
-        # "foo": foo()
+        "MobileNetV2": get_mobilenet_v2_model(img_shape),
+        "InceptionResNetV2": get_inception_res_net_v2_model(img_shape),
+        "InceptionV3": get_inception_v3_model(img_shape)
     }
     model = model_switcher.get(args.model_name)
-    model = apply_classifier(model)
+    model = apply_classifier(model, class_count)
 
     # Display the model and ask whether it is correct
     model.summary()
@@ -93,21 +118,19 @@ def main():
         return
 
     # Train the model
-    initial_epochs = 40
     base_learning_rate = 0.0001
     model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-    history = model.fit(train_ds, epochs=initial_epochs,
-                    validation_data=val_ds)
-    
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    history = model.fit(train_ds, epochs=epochs,
+                        validation_data=val_ds)
+
     acc = history.history['accuracy']
     print("Training accuraccy: ", acc)
     print("Testing the accuracy with test dataset")
-    
 
-    print("Saving the model to " +  args.file_name + ".json and its weights to " + args.file_name + ".h5...")
-    
+    print("Saving the model to " + args.file_name + ".json and its weights to " + args.file_name + ".h5...")
+
     model_json = model.to_json()
     with open(args.file_name + ".json", "w") as json_file:
         json_file.write(model_json)
